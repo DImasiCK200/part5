3e45165f526f3a6437177c5f2351cf159133ba32
ShpkUshnk <zgfzgfzgaf@gmail.com>
2026-03-16 15:17:05 +0700

    5.9

--- src/components/BlogDescription.jsx
+++ src/components/BlogDescription.jsx
@@ -1,11 +1,40 @@
-const BlogDescription = ({ style, blog }) => (
-  <div style={style}>
-    <div>Url: {blog.url}</div>
-    <div>
-      Likes: {blog.likes} <button>Like</button>
+import { useState } from "react";
+import blogService from "../services/blogs";
+
+const BlogDescription = ({ style, blog }) => {
+  const [likes, setLikes] = useState(blog.likes);
+
+  const handleLike = async (blog) => {
+    try {
+      const editBlog = { ...blog, user: blog.user.id, likes: likes + 1 };
+
+      const updatedBlog = await blogService.update(editBlog);
+
+      if (updatedBlog) {
+        setLikes(updatedBlog.likes);
+      }
+    } catch (err) {
+      console.log(err.response.data.error);
+    }
+  };
+
+  return (
+    <div style={style}>
+      <div>Url: {blog.url}</div>
+      <div>
+        Likes: {likes}
+        <button
+          onClick={async () => {
+            await handleLike(blog);
+            setLikes(likes + 1);
+          }}
+        >
+          Like
+        </button>
+      </div>
+      <div>Created by: {blog.user.name}</div>
     </div>
-    <div>Created by: {blog.user.name}</div>
-  </div>
-);
+  );
+};
 
 export default BlogDescription;
